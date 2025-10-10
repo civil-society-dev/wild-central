89c56a079d05a815f120469462684580fa2237ff
Paul Payne <paul@payne.io>
2025-10-10 22:03:24 +0000

    Removes dev data.

--- data/instances/test-cloud/setup/cluster-services/externaldns/configure.sh
+++
@@ -1,3 +0,0 @@
-print_info "Collecting ExternalDNS configuration..."
-
-prompt_if_unset_config "cluster.externalDns.ownerId" "Enter ExternalDNS owner ID (unique identifier for this cluster)" "wild-cloud-$(hostname -s)"
--- data/instances/test-cloud/setup/cluster-services/traefik/configure.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-print_info "Collecting Traefik configuration..."
-
-prompt_if_unset_config "cluster.loadBalancerIp" "Enter load balancer IP address for Traefik" "192.168.1.240"
